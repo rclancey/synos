a3160932fed7a44aef840fe635dc320a656a64de
Ryan Clancey <rclancey@gmail.com>
2021-05-10 09:53:49 -0700

    remove non-go code

--- js/src/components/Desktop/Device/Airplay/DevicePlaylist.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const AirplayDevicePlaylist = ({
-  device,
-}) => {
-  return (<></>);
-};
-
--- js/src/components/Mobile/Device/Apple/DevicePlaylist.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-  
-export const AppleDevicePlaylist = ({
-  device,
-}) => {
-  return (<></>);
-};
